3ddbc2ce62c62969cfd6f7ebc8bda5d4b1c10313
Sebastian Fripp <80719326+irubyx@users.noreply.github.com>
2022-05-17 16:35:10 -0300

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:18
+
+WORKDIR /usr/scr/app
+
+COPY . .
+
+CMD [ "node", "app.js" ]
---
+++ app.js
@@ -0,0 +1,3 @@
+const date = new Date()
+const newDate = `La hora actual es: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+console.log(newDate)
