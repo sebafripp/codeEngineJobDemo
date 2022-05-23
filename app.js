6bc14d7ac023ff1fea285008c1278e276a5b08f8
Sebastian Fripp <irubyxrubyx@gmail.com>
2022-05-23 13:41:00 -0300

    De job a app

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const date = new Date()
-const newDate = `La hora actual es: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-console.log(newDate)
+const express = require("express")
+
+// Constants
+const port = 8080
+const HOST = "0.0.0.0"
+
+// App
+const app = express()
+app.get("/", (req, res) => {
+    let date
+    let newDate
+    while (true) {
+        date = new Date()
+        newDate = `La hora actual es: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+        console.log(newDate)
+    }
+})
+
+app.listen(port, HOST)
+console.log(`Corriendo en http://${HOST}:${port}`)
